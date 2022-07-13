aa75a6ef45a6e7dc76e957e35e10af243f6babe5
Juan Antonio Sauceda <jasgordo@gmail.com>
2022-07-12 20:48:04 -0500

    Update Copyright year

--- source/Image.cpp
+++ source/Image.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Juan A. Sauceda <jasauceda@skibur.com>
+ * Copyright 2022 Juan A. Sauceda <jasauceda@skibur.com>
  *
  * This program is free software; you can redistribute itand /or modify
  * it under the terms of the GNU General Public License as published by
